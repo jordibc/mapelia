a0d3479758b7b60cb1c9718dcc1370a4a9d40634
Jordi <jordi.burguet.castell@gmail.com>
2017-09-22 23:45:13 +0200

    maps.py: minor cosmetics

--- maps.py
+++ maps.py
@@ -37,7 +37,7 @@ Patch = namedtuple('Patch', ['points', 'faces'])
 Point = namedtuple('Point', ['pid', 'x', 'y', 'z'])
 
 def ansi(n, bold=False):
-    "Return function that escapes text with ANSI color n."
+    "Return function that escapes text with ANSI color n"
     return lambda txt: '\x1b[%d%sm%s\x1b[0m' % (n, ';1' if bold else '', txt)
 
 black, red, green, yellow, blue, magenta, cyan, white = map(ansi, range(30, 38))
